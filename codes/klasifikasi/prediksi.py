8237c04c95260bbee26e98afae820872cc6b215d
syahdeee <100667458+syahdeee@users.noreply.github.com>
2023-08-17 11:12:09 +0700

    add new

--- codes/klasifikasi/prediksi.py
+++ codes/klasifikasi/prediksi.py
@@ -3,22 +3,18 @@ import tensorflow as tf
 import os
 import tempfile
 from sklearn.preprocessing import MinMaxScaler
-import tensorflow_addons as tfa
+import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 import io
 
 
-def predict_mlp():
-    # Define the custom metric function
-    @tf.function
-    def r_square(y_true, y_pred):
-        SS_res = tf.reduce_sum(tf.square(y_true - y_pred))
-        SS_tot = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)))
-        return 1 - SS_res / (SS_tot + tf.keras.backend.epsilon())
+def reverse_dictionary(original_dict):
+    reversed_dict = {value: key for key, value in original_dict.items()}
+    return reversed_dict
 
-    # Register the custom metric function
-    tf.keras.utils.get_custom_objects()["r_square"] = r_square
 
+def prediksi_klasifikasi():
     nama_data = st.text_input("Masukkan nama data : ")
     # File uploader
     # File uploader
@@ -30,7 +26,7 @@ def predict_mlp():
     if file_pro is not None:
         try:
             if file_pro.name.endswith(".csv"):
-                df = pd.read_csv(file_pro, sep=";")
+                df = pd.read_csv(file_pro, sep=",")
             else:
                 df = pd.read_excel(file_pro)
         except Exception as e:
@@ -55,9 +51,8 @@ def predict_mlp():
             temp_path = temp_file.name
             temp_file.write(uploaded_model.read())
 
-        # Load the model from the temporary file using Keras and custom metric
-        with tf.keras.utils.custom_object_scope({"r_square": r_square}):
-            model = tf.keras.models.load_model(temp_path)
+        # Load the model from the temporary file using Keras
+        model = tf.keras.models.load_model(temp_path)
 
         # Remove the temporary file
         os.remove(temp_path)
@@ -71,8 +66,9 @@ def predict_mlp():
                 # Display the model summary from the string buffer
                 st.text(stream.getvalue())
 
-        st.subheader(f"TENTUKAN FEATURE INPUT DAN OUTPUT DATA {nama_data}")
+    st.subheader(f"TENTUKAN FEATURE INPUT DAN OUTPUT DATA {nama_data}")
 
+    if file_pro is not None:
         options = df.columns
         # Elemen input ditempatkan di kolom pertama ()
         input_nfeatures = st.number_input(
@@ -92,15 +88,21 @@ def predict_mlp():
             options,
         )
 
+        y = df[feature_output]
+        le = LabelEncoder()
+        y_encode = le.fit_transform(y)
+
+        label_encoding_dict = {label: y_encode for label, y_encode in zip(y, y_encode)}
+
+        # Reverse the dictionary to create a new dictionary with keys and values swapped
+        reversed_dict = {int(value): key for key, value in label_encoding_dict.items()}
+
         data = {}
 
         for i in range(len(features_input)):
             min = df[features_input[i]].min()
             max = df[features_input[i]].max()
             data[features_input[i]] = (min, max)
-
-        min_output = df[feature_output].min()
-        max_output = df[feature_output].max()
 
         if input_nfeatures != 0:
             st.subheader("INPUTKAN DATA YANG AKAN DIPREDIKSI :")
@@ -121,24 +123,19 @@ def predict_mlp():
             scaler.fit(dfx)
             x_scaled = scaler.transform(dfx)
 
-            dfy = pd.DataFrame({"output": [min_output, max_output]})
-
-            # Fit the scaler to the data
-            scaler.fit(dfy)
-
-            # prediksi data
-            y_model_test = model.predict(x_scaled)
-
-            # inverse ke nilai normal
-            y_test_denorm = scaler.inverse_transform(y_model_test)
-            hasil = y_test_denorm[-1][0]
+            y_pred = model.predict(x_scaled)
+            y_pred_classes = np.argmax(y_pred, axis=1)
+            hasil = y_pred_classes[-1]
 
             predict = st.button("PREDIKSI")
             if predict:
-                st.write(
-                    f"Hasil prediksi nilai {feature_output} pada data {nama_data} : ",
-                    hasil,
-                )
+                # Look up the key using the reversed dictionary
+                if hasil in reversed_dict:
+                    value = reversed_dict[hasil]
+                    st.write(
+                        f"Hasil prediksi nilai {feature_output} pada data {nama_data} : ",
+                        value,
+                    )
 
 
-predict_mlp()
+prediksi_klasifikasi()
